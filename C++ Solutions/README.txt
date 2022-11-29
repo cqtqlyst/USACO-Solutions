d65711d7b2b15462d81d24daa9c6f4660fda1fe3
cqtqlyst <aayush.gaywala@gmail.com>
2022-11-28 21:12:52 -0800

    GOT STUFF WORKING!

--- C++ Solutions/README.txt
+++
@@ -1,5 +0,0 @@
-will add something soon lmfao
-
-learning c++ rn
-
-cqtqlyst - 10/7/2022
---
+++ C++ Solutions/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World\n";
+
+    int e;
+
+    std::cin >> e;
+
+    std::cout << e;
+}
