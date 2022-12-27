21bf181c61a6ae4d375b23bffc1c9653016adc9f
cqtqlyst <aayush.gaywala@gmail.com>
2022-12-27 13:01:04 -0800

    working on paintingthebarn

--- C++ Solutions/PrefixSums/paintingTheBarnGold/paintingTheBarn.cpp
+++ C++ Solutions/PrefixSums/paintingTheBarnGold/paintingTheBarn.cpp
@@ -24,8 +24,22 @@ int main() {
         prefix[x1][y2]--;
         prefix[x2][y1]--;
         prefix[x2][y2]++;
-    }
+    }    
 
-    
+    for (int i = 0; i<n; i++) {
+        for (int j = 0; j<n; j++) {
+            if (i>0) {
+                prefix[i][j] += prefix[i-1][j];
+            }
+            if (j>0) {
+                prefix[i][j] += prefix[i][j-1];
+            }
+            if (i>0 && j>0) {
+                prefix[i][j] -= prefix[i-1][j-1];
+            }
+            cout << prefix[i][j] << " ";
+         }
+         cout << "\n";
+    }
     
 }
